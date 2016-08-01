59ec55c317849ee5a1e399cffaddfd62ddc152f9
Alberto J. Rubio <yebenes9@gmail.com>
2016-08-01 11:51:34 +0200

    Add empty test

---
+++ src/test/java/io/corbel/lib/config/EmptyTest.java
@@ -0,0 +1,16 @@
+package io.corbel.lib.config;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+/**
+ * @author Alberto J. Rubio
+ */
+public class EmptyTest {
+
+    @Test
+    public void testEmpty() {
+        assertTrue(true);
+    }
+}
